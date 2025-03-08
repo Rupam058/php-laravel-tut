9740a0b821dbb1292cacb332d35ab9d38cb5cb2a
Rupam058 <rupam.irp@gmail.com>
2025-03-08 11:48:39 +0530

    17 - Always Validate. Never Trust the User

--- example/resources/views/jobs/create.blade.php
+++ example/resources/views/jobs/create.blade.php
@@ -18,8 +18,12 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="title" id="title"
                                     class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
                             </div>
+
+                            @error('title')
+                                <p class="text-xs text-red-500 font-semibold mt-2">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -30,18 +34,32 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="salary" id="salary"
                                     class="block min-w-0 grow py-1.5 px-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="$50,000 per year">
+                                    placeholder="$50,000 per year" required>
                             </div>
+                            @error('salary')
+                                <p class="text-xs text-red-500 font-semibold mt-2">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
                 </div>
+
+                {{-- <div class="mt-10">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div> --}}
             </div>
 
 
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
+        <div class="mt-6 flex
+                            items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
             <button type="submit"
                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
